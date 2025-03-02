762828bea0c520ad45062abddd22e2ff45f19e0e
Barış Ünsal <unsal.baris01@gmail.com>
2025-03-02 20:36:16 +0300

    definitions

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ def int_to_roman(num):
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
+    # 1 -> I  5 -> V  10 -> X  50 -> L  100 -> C  500 -> D  1000 -> M
